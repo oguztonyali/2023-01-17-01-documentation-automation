4676af9070457da546c24ded7f0a97317eb27aea
Oguz <oguztonyali0@gmail.com>
2023-01-18 18:17:48 +0300

    UI initial work

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-# Documentation Copy Pasting Automation Project
+from tkinter import *
+import tkinter.ttk as ttk
+
+# Tender Documentation Automation Project
+
+# ---------------------------- Colours ------------------------------------------- #
+DARK = "#222831"
+GRAY = "#393E46"
+BLUE = "#00ADB5"
+WHITE = "#EEEEEE"
+FONT_NAME = "Courier"
+# -------------------------------------------------------------------------------- #
+
+country_list = ['Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe']
+
+window = Tk()
+window.title("Tender Documentation Helper v.0.1")
+window.config(padx=30, pady=30, bg=DARK)
+
+# ---------------------------- UI FIRST PAGE SETUP ------------------------------- #
+
+canvas = Canvas(width=256, height=256, bg=DARK, highlightthickness=0)
+folder_img = PhotoImage(file="folders.png")
+canvas.create_image(128, 128, image=folder_img)
+canvas.grid(row=0, column=0, columnspan=8)
+
+tender_no_label = Label(text="Tender No:", fg=BLUE, bg=DARK, font=(FONT_NAME, 12, "bold"))
+tender_no_label.grid(row=1, column=1)
+
+tender_no_entry = Entry(width=30)
+tender_no_entry.grid(row=1, column=2)
+
+tender_name_label = Label(text="Tender Name:", fg=BLUE, bg=DARK, font=(FONT_NAME, 12, "bold"))
+tender_name_label.grid(row=1, column=4)
+
+tender_name_entry = Entry(width=30)
+tender_name_entry.grid(row=1, column=5)
+
+country_choose = Label(text="Country:", fg=BLUE, bg=DARK, font=(FONT_NAME, 12, "bold"))
+country_choose.grid(row=2, column=1, sticky="W")
+
+country_box = ttk.Combobox(window, width=27)
+country_box.grid(row=2, column=2)
+country_box["values"] = country_list
+
+# -------------------------------------------------------------------------------- #
+
+save_button = Button(text="Save", width=15)
+save_button.grid(row=15, column=1)
+
+clear_tender_button = Button(text="Clear Tender", width=15)
+clear_tender_button.grid(row=15, column=2)
+
+clear_all_button = Button(text="Clear All", width=15)
+clear_all_button.grid(row=15, column=3)
+
+next_button = Button(text="Next", width=15)
+next_button.grid(row=15, column=4)
+
+help_button = Button(text="?", width=5, bg=DARK, font=(FONT_NAME, 12, "bold"))
+help_button.grid(row=15, column=5)
+
+# -------------------------------------------------------------------------------- #
+
+window.mainloop()
