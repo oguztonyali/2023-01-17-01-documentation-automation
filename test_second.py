cbb2515cbd370fc7881b81bb04ef7dfc0e7434b4
Oguz <oguztonyali0@gmail.com>
2023-01-28 14:53:28 +0300

    UI TAB Revision and First Page Adaptation

--- test_second.py
+++ test_second.py
@@ -1,7 +1,12 @@
 import unittest
-import secondpage
+from secondpage import second_page
+
 
 class TestSecondpage(unittest.TestCase):
 
     def test_help_button(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
